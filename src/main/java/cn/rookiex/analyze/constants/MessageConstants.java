402b44c0dc0e048d3a63f2bc3f3d5e0cf9782474
xuzhishun <774590465@qq.com>
2020-12-09 22:07:34 +0800

    数据分析接口折线统计图功能完善

---
+++ src/main/java/cn/rookiex/analyze/constants/MessageConstants.java
@@ -0,0 +1,11 @@
+package cn.rookiex.analyze.constants;
+
+/**
+ * @author rookiex
+ * @date 2020/12/9 17:49
+ * @des
+ */
+public class MessageConstants {
+
+    public static final int CLIENT_ALL_ID = -999;
+}
--- src/main/java/cn/rookiex/analyze/controller/AnalyzeController.java
+++ src/main/java/cn/rookiex/analyze/controller/AnalyzeController.java
@@ -24,14 +24,7 @@ public class AnalyzeController {
     @Autowired
     private AnalyzeService analyzeService;
 
-    @GetMapping(path = "/exams/student")
-    @ResponseBody
-    public String getStudentAllExam(@RequestParam int rank, @RequestParam int sId) {
-        LineResultData analyzeExam = analyzeService.getStudentAllExamResult(sId, rank == 1);
-        return JSONObject.toJSONString(analyzeExam);
-    }
-
-    @GetMapping(path = "/exams/students")
+    @GetMapping(path = "/student/exams")
     @ResponseBody
     public String getStudentAllExam(@RequestParam int rank, @RequestParam int classId, @RequestParam int grade) {
         LineResultData analyzeExam = analyzeService.getClassAllExamResult(classId, grade, rank == 1);
@@ -42,17 +35,25 @@ public class AnalyzeController {
         return s;
     }
 
-    @GetMapping(path = "/exam/class")
+    /**
+     * 获得班级单次考试的成绩分析柱状图
+     * @param examId 考试id
+     * @param classId 班级id
+     * @param grade 年级
+     * @return 单次考试成绩数据
+     */
+    @GetMapping(path = "/class/exam")
     @ResponseBody
-    public String getClassAllExamHistory(@RequestParam String needPrams, @RequestParam int classId, @RequestParam int grade) {
-        if (needPrams == null || needPrams.isEmpty()){
+    public String getClassExamResult(@RequestParam int examId, @RequestParam int classId, @RequestParam int grade) {
+        return null;
+    }
+
+    @GetMapping(path = "/class/exams")
+    @ResponseBody
+    public String getClassAllExamHistory(@RequestParam int classId, @RequestParam int grade) {
             ExamResults analyzeExam = analyzeService.getClassAllExamHistory(classId, grade);
             return JSONObject.toJSONString(analyzeExam);
-        }else {
-            String[] prams = needPrams.split(",");
-            ExamResults analyzeExam = analyzeService.getClassAllExamHistoryWithPrams(classId, grade, prams);
-            return JSONObject.toJSONString(analyzeExam);
-        }
-
+//            ExamResults analyzeExam = analyzeService.getClassAllExamHistoryWithPrams(classId, grade, prams);
+//            return JSONObject.toJSONString(analyzeExam);
     }
 }
